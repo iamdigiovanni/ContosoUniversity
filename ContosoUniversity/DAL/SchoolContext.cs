5b17f63189ba1d16ae9f2d1d5749829b36c41d08
iamdigiovanni <51376713+iamdigiovanni@users.noreply.github.com>
2020-01-12 19:20:04 -0600

    Enabled connection resiliency and command interception for transient errors

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("School Context")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        // Developers disagree about whether table names should be pluralized or singular.
+        // In this tutorial we will use Student, Course, and Enrollment instead of
+        // Strudents, Courses, and Enrollments bu using remove pluralizing table name conventions
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
